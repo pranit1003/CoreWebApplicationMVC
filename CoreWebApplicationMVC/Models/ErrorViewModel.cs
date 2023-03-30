2dc9b3265250de1a04a1b17bfcd37c8c9d736521
pranit1003 <pranitchougule77@gmail.com>
2023-03-30 08:36:32 +0530

    Add project files.

---
+++ CoreWebApplicationMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreWebApplicationMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
